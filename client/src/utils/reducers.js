281cfc6400f5d545a99506e75202347dca0cb029
sebastian <sebastian bowen>
2021-06-15 03:43:19 -0600

    global state now changing and passing through provider

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,36 @@
+
+// react hook to handle reducer function's
+import  { useReducer } from 'react';
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from "./actions";
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        // if action type value is the value of `UPDATE_PRODUCTS`, RETURN A NEW    state object with an updated products array
+          case UPDATE_PRODUCTS:
+              return {
+                  ...state,
+                  products: [...action.products],
+              };
+              // if action type value is the value of UPDATE_CATEGORIES, RETURN A NEW state object with an updated categories array
+               case UPDATE_CATEGORIES: 
+               return {
+                   ...state,
+                   categories: [...action.categories]
+               };
+               case UPDATE_CURRENT_CATEGORY:
+                   return {
+                       ...state,
+                        currentCategory: action.currentCategory
+                   };
+           default:
+               return state;
+    }
+};
+
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+};
