5e8ce8af453f639ea6ae71289b9e06af206386e1
sebastian <sebastian bowen>
2021-06-15 15:00:05 -0600

    global state now up and running , components refrectored

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_CATEGORIES} from "../../utils/queries";
 import { useStoreContext } from "../../utils/GlobalState";
-import { UPDATE_CATEGORIES /*,   UPDATE_CURRENT_CATEGORY*/  } from "../../utils/actions";
+import { UPDATE_CATEGORIES ,   UPDATE_CURRENT_CATEGORY  } from "../../utils/actions";
 
-function CategoryMenu({ setCategory }) {
+function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
   const { categories } = state;
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
@@ -18,8 +18,15 @@ function CategoryMenu({ setCategory }) {
           categories: categoryData.categories
         });
     }
-    console.log(categoryData);
+    //console.log(categoryData);
   }, [categoryData, dispatch ]);
+
+  const handleClick = id => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    });
+  };
   
   
 
@@ -30,7 +37,7 @@ function CategoryMenu({ setCategory }) {
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            handleClick(item._id);
           }}
         >
           {item.name}
