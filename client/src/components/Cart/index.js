65d394ba8dbefe8deefc60849826e228b90f0878
sebastian <sebastian bowen>
2021-06-16 18:44:09 -0600

    shopping cart functionality all done

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,29 +2,68 @@ import React from 'react' ;
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
-
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART } from '../../utils/actions';
+import { valueFromASTUntyped } from 'graphql';
 const Cart = () => {
-    return (
-        <div className='cart'>
-            <div className="close">[close]</div>
-            <h2>Shopping Cart</h2>
-            <div>
-                <CartItem item= {{name: 'Camera', image:'camera.jpg', price:5, purchaseQuantity:3}} />
-                <CartItem item={{name: 'Soap', image:'soap.jpg', price:6, purchaseQuantity:4}} />
+    const [state, dispatch] = useStoreContext();
 
-                <div className="flex-row space-between">
-                    <strong>Total: $0</strong>
-                    {
-                        Auth.loggedIn() ?
-                        <button>
-                        Checkout
-                        </button>
-                        :
-                        <span>(log in to check out)</span>
-                    }
-                </div>
+    function toggleCart() {
+        dispatch({ type: TOGGLE_CART });
+    }
+
+    if (!state.cartOpen) {
+        return (
+            <div className="cart-closed" onClick={toggleCart}>
+                <span
+                role="img"
+                aria-label="trash"
+                >🛒 </span>
             </div>
-        </div>
+        );
+    };
+   // console.log(state);
+
+   function calculateTotal() {
+       let sum = 0;
+       state.cart.forEach(item => {
+           sum += item.price * item.purchaseQuantity;
+       });
+       return sum.toFixed(2);
+
+   }
+
+    return (
+       <div className="cart">
+           <div className="close" onClick={toggleCart}>[close]</div>
+           <h2>Shopping Cart</h2> 
+           {state.cart.length ? (
+         <div>
+          {state.cart.map(item => (
+              <CartItem key={item._id} item={item} />
+          ))} 
+          <div className="flex-row space-between">
+          <strong>Total: ${calculateTotal()}</strong>
+          {
+              Auth.loggedIn() ?
+              <button>
+                  Checkout
+              </button>
+              :
+              <span>(log in to check out)</span>
+          }    
+          </div>  
+         </div>
+           ) : ( 
+               <h3> 
+               <span role="img" aria-label="shocked">
+                   😱
+               </span>
+               Nothing in your cart Yet!
+               </h3>
+
+           )}     
+            </div>
     );
 };
 
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -7,11 +7,13 @@ const { Provider }  = StoreContext;
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useProductReducer({
         products: [],
+        cart: [],
+        cartOpen: false,
         categories: [],
         currentCategory: '',
     });
     // use this to confirm it works!
-    console.log(state);
+   // console.log(state);
     // if {...props} were not provided nothing would render
     return <Provider value={[state, dispatch]} {...props} />
     
